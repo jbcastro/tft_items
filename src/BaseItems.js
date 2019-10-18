1737637822668d578ad209447e7452b9bb1c2521
orbtl <twinsendude@gmail.com>
2019-10-18 16:45:06 -0700

    filling in BaseItems dictionary

--- src/BaseItems.js
+++ src/BaseItems.js
@@ -1,7 +1,13 @@
- const BaseItems={
-    "BF":{name:"B.F Sword", id:"BF"},
-    "CV":{name:"Chain Vest", id:"CV"}
-}
+ const BaseItems=[
+    "BF": {id: "BF", name: "B.F. Sword"},
+    "CV": {id: "CV", name: "Chain Vest"},
+    "GB": {id: "GB", name: "Giant's Belt"},
+    "NC": {id: "NC", name: "Negatron Cloak"},
+    "NLR": {id: "NLR", name: "Needlessly Large Rod"},
+    "RB": {id: "RB", name: "Recurve Bow"},
+    "Spat": {id: "Spat", name: "Spatula"},
+    "Tear": {id: "Tear", name: "Tear of the Goddess"}
+ ]
 
 export default BaseItems
 
